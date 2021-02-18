60fb20c523abd5cde6143e7065022b2c17b3cd21
unknown <kkg.rasul@gmail.com>
2021-02-18 13:04:25 +0600

    Updated DocsCard1, DocsList, Header, Updated backgroundColor

--- src/containers/Docs/DocsList.jsx
+++ src/containers/Docs/DocsList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import DocsCard from './DocsCard';
+import DocsCard1 from './DocsCard1';
+
 
 const DocsList = () => {
     return (
         <div>
-            <DocsCard  />
+            <DocsCard1/>
         </div>
     );
 };
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,297 +1,3 @@
-// import React from 'react';
-// import { fade, makeStyles } from '@material-ui/core/styles';
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import IconButton from '@material-ui/core/IconButton';
-// import Typography from '@material-ui/core/Typography';
-// import InputBase from '@material-ui/core/InputBase';
-// import Badge from '@material-ui/core/Badge';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import Menu from '@material-ui/core/Menu';
-// import MenuIcon from '@material-ui/icons/Menu';
-// import SearchIcon from '@material-ui/icons/Search';
-// import AccountCircle from '@material-ui/icons/AccountCircle';
-// import MailIcon from '@material-ui/icons/Mail';
-// import NotificationsIcon from '@material-ui/icons/Notifications';
-// import MoreIcon from '@material-ui/icons/MoreVert';
-// import Logo from '../../assets/makers.svg';
-
-// const useStyles = makeStyles((theme) => ({
-//     grow: {
-//         flexGrow: 1,
-//     },
-//     menuButton: {
-//         marginRight: theme.spacing(2),
-//     },
-//     title: {
-//         display: 'none',
-//         [theme.breakpoints.up('sm')]: {
-//             display: 'block',
-//         },
-//         fontFamily: '"Poppins", sans-serif',
-//     },
-//     search: {
-//         position: 'relative',
-//         borderRadius: theme.shape.borderRadius,
-//         backgroundColor: fade(theme.palette.common.white, 0.15),
-//         '&:hover': {
-//             backgroundColor: fade(theme.palette.common.white, 0.25),
-//         },
-//         marginRight: theme.spacing(2),
-//         marginLeft: 0,
-//         width: '100%',
-//         [theme.breakpoints.up('sm')]: {
-//             marginLeft: theme.spacing(4),
-//             width: 'auto',
-//         },
-//     },
-//     searchIcon: {
-//         padding: theme.spacing(0, 2),
-//         height: '100%',
-//         position: 'absolute',
-//         pointerEvents: 'none',
-//         display: 'flex',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//     },
-//     inputRoot: {
-//         color: 'inherit',
-//     },
-//     inputInput: {
-//         fontFamily: '"Poppins", sans-serif',
-//         padding: theme.spacing(1, 1, 1, 0),
-//         // vertical padding + font size from searchIcon
-//         paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-//         transition: theme.transitions.create('width'),
-//         width: '100%',
-//         [theme.breakpoints.up('md')]: {
-//             width: '20ch',
-//         },
-//     },
-//     sectionDesktop: {
-//         display: 'none',
-//         [theme.breakpoints.up('md')]: {
-//             display: 'flex',
-//         },
-//     },
-//     sectionMobile: {
-//         display: 'flex',
-//         [theme.breakpoints.up('md')]: {
-//             display: 'none',
-//         },
-//     },
-
-//     appBar: {
-//         fontFamily: '"Poppins", sans-serif',
-//         backgroundColor: '#545E80',
-//         // boxShadow: '0px 0px 0px 0px'
-//     }
-// }));
-
-// export default function Header() {
-//     const classes = useStyles();
-//     const [anchorEl, setAnchorEl] = React.useState(null);
-//     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-//     const isMenuOpen = Boolean(anchorEl);
-//     const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-//     const handleProfileMenuOpen = (event) => {
-//         setAnchorEl(event.currentTarget);
-//     };
-
-//     const handleMobileMenuClose = () => {
-//         setMobileMoreAnchorEl(null);
-//     };
-
-//     const handleMenuClose = () => {
-//         setAnchorEl(null);
-//         handleMobileMenuClose();
-//     };
-
-//     const handleMobileMenuOpen = (event) => {
-//         setMobileMoreAnchorEl(event.currentTarget);
-//     };
-
-//     const menuId = 'primary-search-account-menu';
-//     const renderMenu = (
-//         <Menu
-//             anchorEl={anchorEl}
-//             anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-//             id={menuId}
-//             keepMounted
-//             transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-//             open={isMenuOpen}
-//             onClose={handleMenuClose}
-//         >
-//             <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-//             <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-//         </Menu>
-//     );
-
-//     const mobileMenuId = 'primary-search-account-menu-mobile';
-//     const renderMobileMenu = (
-//         <Menu
-//             anchorEl={mobileMoreAnchorEl}
-//             anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-//             id={mobileMenuId}
-//             keepMounted
-//             transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-//             open={isMobileMenuOpen}
-//             onClose={handleMobileMenuClose}
-//         >
-//             <MenuItem>
-//                 <IconButton aria-label="show 4 new mails" color="inherit">
-//                     <Badge badgeContent={4} color="secondary">
-//                         <MailIcon />
-//                     </Badge>
-//                 </IconButton>
-//                 <p>Messages</p>
-//             </MenuItem>
-//             <MenuItem>
-//                 <IconButton aria-label="show 11 new notifications" color="inherit">
-//                     <Badge badgeContent={11} color="secondary">
-//                         <NotificationsIcon />
-//                     </Badge>
-//                 </IconButton>
-//                 <p>Notifications</p>
-//             </MenuItem>
-//             <MenuItem onClick={handleProfileMenuOpen}>
-//                 <IconButton
-//                     aria-label="account of current user"
-//                     aria-controls="primary-search-account-menu"
-//                     aria-haspopup="true"
-//                     color="inherit"
-//                 >
-//                     <AccountCircle />
-//                 </IconButton>
-//                 <p>Profile</p>
-//             </MenuItem>
-//         </Menu>
-//     );
-
-//     return (
-//         <div className={classes.grow}>
-//             <AppBar position="static" className={classes.appBar}>
-//                 <Toolbar>
-//                     <IconButton
-//                         edge="start"
-//                         className={classes.menuButton}
-//                         color="inherit"
-//                         aria-label="open drawer"
-//                     >
-//                         <MenuIcon />
-//                     </IconButton>
-//                     <Typography className={classes.title} variant="h6" noWrap>
-//                     <img src={Logo} alt="logo" fill="black" style={{color: "#f50057"}} /> Documentation
-//           </Typography>
-//                     <div className={classes.search}>
-//                         <div className={classes.searchIcon}>
-//                             <SearchIcon />
-//                         </div>
-//                         <InputBase
-//                             placeholder="Search…"
-//                             classes={{
-//                                 root: classes.inputRoot,
-//                                 input: classes.inputInput,
-//                             }}
-//                             inputProps={{ 'aria-label': 'search' }}
-//                         />
-//                     </div>
-//                     <div className={classes.grow} />
-//                     <div className={classes.sectionDesktop}>
-//                         <IconButton aria-label="show 4 new mails" color="inherit">
-//                             <Badge badgeContent={4} color="secondary">
-//                                 <MailIcon />
-//                             </Badge>
-//                         </IconButton>
-//                         <IconButton aria-label="show 17 new notifications" color="inherit">
-//                             <Badge badgeContent={17} color="secondary">
-//                                 <NotificationsIcon />
-//                             </Badge>
-//                         </IconButton>
-//                         <IconButton
-//                             edge="end"
-//                             aria-label="account of current user"
-//                             aria-controls={menuId}
-//                             aria-haspopup="true"
-//                             onClick={handleProfileMenuOpen}
-//                             color="inherit"
-//                         >
-//                             <AccountCircle />
-//                         </IconButton>
-//                     </div>
-//                     <div className={classes.sectionMobile}>
-//                         <IconButton
-//                             aria-label="show more"
-//                             aria-controls={mobileMenuId}
-//                             aria-haspopup="true"
-//                             onClick={handleMobileMenuOpen}
-//                             color="inherit"
-//                         >
-//                             <MoreIcon />
-//                         </IconButton>
-//                     </div>
-//                 </Toolbar>
-//             </AppBar>
-//             {renderMobileMenu}
-//             {renderMenu}
-//             <div>
-//                 <h1>1111</h1>
-//             </div>
-//         </div>
-//     );
-// }
-
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
-// import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
-
-// const useStyles = makeStyles((theme) => ({
-//     header: {
-//         backgroundColor: "black!important",
-//         color: "white",
-//         flexGrow: 1,
-//     },
-//     menuButton: {
-//         marginRight: theme.spacing(2),
-//     },
-//     title: {
-//         flexGrow: 1,
-//     }
-// }));
-
-// export default function ButtonAppBar() {
-//     const classes = useStyles();
-
-//     return (
-//         <div >
-//             <AppBar className={classes.header} position="static">
-//                 <Toolbar>
-//                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-//                         <MenuIcon />
-//                     </IconButton>
-//                     <Typography variant="h6" className={classes.title}>
-//                         <Button><span style={{color: 'white'}}>maker</span><span style={{backgroundColor: "white", color: "black"}}>s</span><span style={{color: 'yellow'}}>Documentation</span></Button> 
-//                     </Typography>
-//                     <Button color="inherit">Login</Button>
-//                     <Typography className={classes.menuButton}>Главная</Typography>
-//                     <Typography className={classes.menuButton}>Главная</Typography>
-//                     <Typography className={classes.menuButton}>Главная</Typography>
-//                     <Typography className={classes.menuButton}>Главная</Typography>
-
-
-//                 </Toolbar>
-//             </AppBar>
-//         </div>
-//     );
-// }
-
 import React from 'react';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -302,12 +8,11 @@ import InputBase from '@material-ui/core/InputBase';
 import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-// import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     grow: {
@@ -356,9 +61,7 @@ const useStyles = makeStyles((theme) => ({
         paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
         transition: theme.transitions.create('width'),
         width: '100%',
-            [theme.breakpoints.up('md')]: {
-                width: '20ch',
-            },
+
     },
     sectionDesktop: {
         display: 'none',
@@ -432,18 +135,10 @@ export default function Header() {
             open={isMobileMenuOpen}
             onClose={handleMobileMenuClose}
         >
-            <MenuItem>
-                <Typography>Documentation</Typography>
-                {/* <IconButton aria-label="show 4 new mails" color="inherit">
-                    <Badge badgeContent={4} color="secondary">
-                        <MailIcon />
-                    </Badge>
-                </IconButton> */}
-                {/* <p>Documentation</p> */}
-            </MenuItem>
+
             <MenuItem>
                 <IconButton aria-label="show 11 new notifications" color="inherit">
-                    
+
                     <Badge badgeContent={11} color="secondary">
                         <NotificationsIcon />
                     </Badge>
@@ -468,39 +163,35 @@ export default function Header() {
         <div className={classes.grow}>
             <AppBar className={classes.header} position="static">
                 <Toolbar>
-                    <IconButton
-                        edge="start"
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="open drawer"
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography className={classes.title} variant="h6" noWrap>
-                        <span style={{ color: 'white' }}>maker</span><span style={{ backgroundColor: "white", color: "black" }}>s</span><span style={{ color: 'yellow' }}>Documentation</span>
-                    </Typography>
+
+                    <Link to="/" style={{ textDecoration: "none" }}>
+                        <IconButton>
+                            <Typography className={classes.title} variant="h6" noWrap>
+                                <span style={{ color: 'white' }}>maker</span><span style={{ backgroundColor: "white", color: "black" }}>s</span><span style={{ color: 'yellow' }}>Documentation</span>
+                            </Typography>
+                        </IconButton>
+                    </Link>
 
                     <div className={classes.grow} />
 
-                    <div  className={classes.sectionDesktop}>
-                    <IconButton aria-label="show 4 new mails"   color="inherit">
-                            {/* <Badge badgeContent={4} color="secondary">
-                                <MailIcon />
-                            </Badge> */}
-                            <div style={{display: 'flex', flexDirection: "column"}}><Typography>Главная</Typography><div style={{backgroundColor: "yellow", width: "30px", height: "3px"}}/></div>
-                            
+                    <div className={classes.sectionDesktop}>
+                        <IconButton aria-label="show 4 new mails" color="inherit">
+                            <div style={{ display: 'flex', flexDirection: "column" }}>
+                                <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                                    <Typography>Главная</Typography>
+                                </Link>
+                            </div>
+
                         </IconButton>
                         <IconButton aria-label="show 4 new mails" color="inherit">
-                            {/* <Badge badgeContent={4} color="secondary">
-                                <MailIcon />
-                            </Badge> */}
+                            <Link to="/detail" style={{ textDecoration: "none", color: "white" }}>
                             <Typography>Документация</Typography>
+                            </Link>
                         </IconButton>
                         <IconButton aria-label="show 17 new notifications" color="inherit">
-                            {/* <Badge badgeContent={17} color="secondary">
-                                <NotificationsIcon />
-                            </Badge> */}
-                            <Typography>Добавить</Typography>
+                            <Link to="/admin" style={{ textDecoration: "none", color: "white" }}>
+                                <Typography>Добавить</Typography>
+                            </Link>
                         </IconButton>
                         <div className={classes.search}>
                             <div className={classes.searchIcon}>
@@ -516,16 +207,6 @@ export default function Header() {
                                 inputProps={{ 'aria-label': 'search' }}
                             />
                         </div>
-                        <IconButton
-                            edge="end"
-                            aria-label="account of current user"
-                            aria-controls={menuId}
-                            aria-haspopup="true"
-                            onClick={handleProfileMenuOpen}
-                            color="inherit"
-                        >
-                            <AccountCircle />
-                        </IconButton>
                     </div>
                     <div className={classes.sectionMobile}>
                         <IconButton
